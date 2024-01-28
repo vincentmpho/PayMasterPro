2f571a594e2399fbc01966783998c560f78d40a8
Tshindimpho <mudzu13@gmail.com>
2024-01-28 19:21:31 +0200

    updated

--- PayMasterPro/Controllers/DepartmentController.cs
+++ PayMasterPro/Controllers/DepartmentController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PayMasterPro.Models;
 
 namespace PayMasterPro.Controllers
 {
     public class DepartmentController : Controller
     {
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
+            List<Department> list = new List<Department>();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("");
             return View();
         }
 
